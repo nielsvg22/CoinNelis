cb3b39fe4c751999946458d42d4eed4d766d11fc
Niels van Gortel <niels.vangortel@student.hu.nl>
2022-04-12 12:27:46 +0200

    Files

---
+++ pages/Dashboard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from '../Compenents/Header'
+import styled from 'styled-components'
+import Main from '../Compenents/Main'
+import Sidebar from '../Compenents/Sidebar'
+
+
+const Dashboard = ({ address }) => {
+  return (
+    <Wrapper>
+      <Sidebar />
+      <MainContainer>
+        <Header  walletAddress={address}/>
+        <Main />
+      </MainContainer>
+    </Wrapper>
+  )
+
+}
+
+export default Dashboard
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  overflow: hidden;
+`
+
+const MainContainer = styled.div`
+  flex: 1;
+`
